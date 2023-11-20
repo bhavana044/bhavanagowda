dfcc66ccb464b37e45a26d6ce565089884f39906
bhavana044 <bhavanamr2142003@gmail.com>
2023-11-20 14:35:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import Header from './Components/Header'
-import Body from './Components/Body'
-import Footer from './Components/Footer'
+// import React from 'react'
+// import Header from './Components/Header'
+// import Body from './Components/Body'
+// import Footer from './Components/Footer'
 
-function App () {
+// function App () {
+//   return (
+//     <div>
+//       <Header></Header>
+//       <Body></Body>
+//       <Footer></Footer>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import React from 'react'
+import Navigation from './Components/Navigation'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import TodoList from './Components/TodoList'
+
+import {BrowserRouter as Router ,Routes, Route} from
+ 'react-router-dom'
+const App = () => {
   return (
     <div>
-      <Header></Header>
-      <Body></Body>
-      <Footer></Footer>
+      <Router>
+        <div>
+            <Navigation/>
+        </div>
+    
+    
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/todolist'element={<TodoList />} />
+    </Routes>
+    </Router>
     </div>
   )
 }
-
 export default App
---
+++ src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+     <phone number>+9844303035</phone><br/>
+     <email>bhavanamr2142003@gmail.com</email><br/>
+     <socialmedia>github:1SV21CS011@shrideviengineering.org</socialmedia><br/> 
+    </div>
+  )
+}
+
+export default Contact
